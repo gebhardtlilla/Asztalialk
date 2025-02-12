ae8bbe77ec54822bb295e9da5a86694dc07fbb8e
Gebhardt Lilla <gebhardt.lilla@ckik.loc>
2025-02-12 11:29:45 +0100

    0212

---
+++ 2025_02_07 Datagriedview/2025_02_07 Datagriedview/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace _2025_02_07_Datagriedview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tablaDGV = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tablaDGV
+            // 
+            this.tablaDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablaDGV.Location = new System.Drawing.Point(229, 12);
+            this.tablaDGV.Name = "tablaDGV";
+            this.tablaDGV.Size = new System.Drawing.Size(661, 426);
+            this.tablaDGV.TabIndex = 0;
+            this.tablaDGV.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tablaDGV_CellClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Fájlbeolvasás";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(27, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tablaDGV);
+            this.Name = "Form1";
+            this.Text = "Táblázat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tablaDGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tablaDGV;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 2025_02_07 Datagriedview/2025_02_07 Datagriedview/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2025_02_07_Datagriedview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TablaBeallitasok();
+            TablaAdatok();
+        }
+
+        private int aktsor = 0;
+        private void TablaAdatok()
+        {
+            tablaDGV.Rows[0].Cells[0].Value = "0,0";
+
+            tablaDGV.Rows[0].HeaderCell.Value = "12222";
+            //tablaDGV.Columns[0].HeaderCell.Value = "név";
+            tablaDGV.Columns[0].HeaderText = "név";
+
+            
+        }
+
+        void TablaBeallitasok()
+        {
+            tablaDGV.RowCount = 10;
+            tablaDGV.ColumnCount = 5;
+
+            int magassag = 30;
+            //int szelesseg = 30;
+            SorMagassagBeallitasa(magassag);
+            //OszlopSzelessegBeallitasa(szelesseg);
+            //tablaDGV.ColumnHeadersVisible = false;
+            //tablaDGV.RowHeadersVisible = false;
+            //tablaDGV.Width = tablaDGV.ColumnCount * szelesseg + 3;
+            //tablaDGV.Height = tablaDGV.RowCount * magassag + 3;
+
+        }
+
+        private void OszlopSzelessegBeallitasa(int szelesseg)
+        {
+            for (int i = 0; i < tablaDGV.ColumnCount; i++)
+            {
+                tablaDGV.Columns[i].Width = szelesseg;
+            }
+        }
+
+        private void SorMagassagBeallitasa(int magassag)
+        {
+            for (int i = 0; i < tablaDGV.RowCount; i++)
+            {
+                tablaDGV.Rows[i].Height = magassag;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Fajlbeolvasas("eredmenyek.csv");
+        }
+
+        private void Fajlbeolvasas(string path)
+        {
+            //StreamReader f = new StreamReader(path);
+            //f.Close();
+            string[] sorok = File.ReadAllLines(path);
+            FejlecbeIras(sorok[0]);
+            AdatokKiiratasa(sorok);
+        }
+
+        private void AdatokKiiratasa(string[] sorok)
+        {
+            tablaDGV.RowCount = sorok.Length - 1;
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                string[] st = sorok[i].Split(';');
+                for (int j = 0; j < st.Length; j++)
+                {
+                    tablaDGV.Rows[i-1].Cells[j].Value = st[j];
+                }
+            }
+        }
+
+        private void FejlecbeIras(string fejlec)
+        {
+            string[] st = fejlec.Split(';');
+            tablaDGV.ColumnCount = st.Length;
+            for (int i = 0; i < st.Length; i++)
+                tablaDGV.Columns[i].HeaderCell.Value = st[i];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tablaDGV.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+        }
+
+        private void tablaDGV_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int sor = tablaDGV.CurrentCell.RowIndex;
+            int oszlop = tablaDGV.CurrentCell.ColumnIndex;
+            Sorszinezes(sor, Color.Red);
+            Sorszinezes(aktsor, Color.White);
+            aktsor = sor;
+            tablaDGV.ClearSelection();
+            //MessageBox.Show($"{sor} {oszlop}");
+        }
+
+        private void Sorszinezes(int sor, Color szin)
+        {
+            for (int i = 0; i < tablaDGV.ColumnCount; i++)
+            {
+
+                tablaDGV.Rows[sor].Cells[i].Style.BackColor = szin;
+            }
+        }
+    }
+}
